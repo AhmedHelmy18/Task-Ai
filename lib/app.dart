92f8718fd644ba95bb9206c7c01adae0b13450c7
Ahmed Helmy <125504430+AhmedHelmy18@users.noreply.github.com>
2026-02-23 17:32:26 +0200

    firebase (#1)

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TaskAi extends StatelessWidget {
+  const TaskAi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
